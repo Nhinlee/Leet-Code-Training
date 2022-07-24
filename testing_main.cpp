6134cb1bd93b4b6a42dc8c18fe7bb960c8d37f7c
Nhin <nhinlechi@gmail.com>
2022-07-24 11:00:59 +0700

    Update testing_main.cpp

--- testing_main.cpp
+++ testing_main.cpp
@@ -27,10 +27,83 @@ public:
 
         return rs;
     }
+
+    char repeatedCharacter(string s) {
+        vector<int> flags(26, 0);
+        for (auto c: s) {
+            flags[c - 'a']++;
+            if (flags[c - 'a'] > 1) return c;
+        }
+        return 'a';
+    }
+
+    int equalPairs(vector<vector<int>> &grid) {
+        int n = grid.size(), rs = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (equalPairs(grid, i, j)) rs++;
+            }
+        }
+        return rs;
+    }
+
+    bool equalPairs(vector<vector<int>> &grid, int i, int j) {
+        for (int k = 0; k < grid.size(); k++) {
+            if (grid[i][k] != grid[k][j]) return false;
+        }
+        return true;
+    }
 };
 
-int main() {
-    Solution *s = new Solution();
-    vector<int> nums{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1};
-    cout << s->longestOnes(nums, 1);
-}
+typedef pair<string, int> Food;
+
+struct foodCMP {
+    bool operator()(const Food a, const Food b) const {
+        if (a.second != b.second) return a.second > b.second;
+        return a.first < b.first;
+    }
+};
+
+class FoodRatings {
+
+    map<string, set<Food, foodCMP>> menu;
+    map<string, string> food_cuisines;
+    map<string, int> foodsMap;
+
+public:
+    FoodRatings(vector<string> &foods, vector<string> &cuisines, vector<int> &ratings) {
+        int n = foods.size();
+        for (int i = 0; i < n; i++) {
+            menu[cuisines[i]].insert(Food{foods[i], ratings[i]});
+            food_cuisines[foods[i]] = cuisines[i];
+            foodsMap[foods[i]] = ratings[i];
+        }
+    }
+
+    void changeRating(string food, int newRating) {
+        auto cuisines = food_cuisines[food];
+        auto oldRating = foodsMap[food];
+        menu[cuisines].erase(Food{food, oldRating});
+        menu[cuisines].insert(Food{food, newRating});
+        foodsMap[food] = newRating;
+    }
+
+    string highestRated(string cuisine) {
+        return menu[cuisine].begin()->first;
+    }
+};
+
+//int main() {
+//    Solution *s = new Solution();
+//    vector<string> foods{"emgqdbo", "jmvfxjohq", "qnvseohnoe", "yhptazyko", "ocqmvmwjq"};
+//    vector<string> cuisines{"snaxol", "snaxol", "snaxol", "fajbervsj", "fajbervsj"};
+//    vector<int> ratings{9, 13, 7, 16, 10, 17, 16, 17};
+//    FoodRatings *f = new FoodRatings(foods, cuisines, ratings);
+//    f->changeRating("qnvseohnoe", 11);
+//    f->highestRated("fajbervsj");
+//    f->changeRating("emgqdbo", 3);
+//    f->changeRating("jmvfxjohq", 9);
+//    f->changeRating("emgqdbo", 14);
+//    f->highestRated("fajbervsj");
+//    f->highestRated("snaxol");
+//}
